7c3ebdc29c3501101c405a564a79f05dd6c7dab5
jarkad <jarkad@jarkad.net.eu.org>
2025-09-15 14:08:50 +0200

    testing: add tests for /register_scan endpoint

--- door_tracker/webui/tests.py
+++ door_tracker/webui/tests.py
@@ -1,1 +1,34 @@
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse
+
+from .models import Scanner, Tag
+
+
+class RegisterScanTests(TestCase):
+    def test_register_thrice(self):
+        user = User.objects.create(username='test', first_name='Test', last_name='User')
+        scanner = Scanner.objects.create(id='scanner', name='Test Scanner')
+        tag = Tag.objects.create(name='test', owner=user)
+        tag_id = 'DEADBEEF'
+
+        res = self.client.post(
+            reverse('register_scan'),
+            {'device_id': scanner.id, 'card_id': tag_id},
+        )
+        self.assertEqual(res.json()['state'], 'register')
+
+        tag.refresh_from_db()
+        self.assertEqual(tag.tag, tag_id)
+
+        res = self.client.post(
+            reverse('register_scan'),
+            {'device_id': scanner.id, 'card_id': tag.tag},
+        )
+        self.assertEqual(res.json()['state'], 'checkin')
+
+        res = self.client.post(
+            reverse('register_scan'),
+            {'device_id': scanner.id, 'card_id': tag.tag},
+        )
+        self.assertEqual(res.json()['state'], 'checkout')
