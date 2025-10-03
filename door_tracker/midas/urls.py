8fbafe3d9c64b8436f6ed91c86b5bc0ef9edfa80
andrei <badea.andrei1908@gmail.com>
2025-10-03 13:28:55 +0200

    feat(webui): add new user dashboard with new database logic
    change from a list view to a table view and modified the backend/css logic

--- door_tracker/midas/urls.py
+++ door_tracker/midas/urls.py
@@ -3,5 +3,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
+    path('', views.index, name='index'),
+    # path('delete_tag', views.delete_tag, name='delete_tag'),
+    # path('rename_tag', views.rename_tag, name='rename_tag'),
+    # path('fuel_guage', views.fuel_guage, name='fuel_guage'),
+    # path('healthcheck', views.healthcheck, name='healthcheck'),
+    path('login', views.LogIn.as_view(), name='login'),
+    path('logout', views.logout_view, name='logout'),
+    # path('sign_up', views.sign_up, name='sign_up'),
+    # path('user_profile', views.user_profile, name='user_profile'),
+    # path('user_statistics', views.user_statistics, name='user_statistics'),
+    # path('user_tags', views.user_tags, name='user_tags'),
+    # path('export_user', views.export_user_logs, name='export_user'),
+    # path('auto_checkout', views.auto_checkout, name='auto_checkout'),
+    # path('edit_profile', views.edit_profile, name='edit_profile'),
+    # path('register_scan', views.register_scan, name='register_scan'),
 ]
--- door_tracker/midas/views.py
+++ door_tracker/midas/views.py
@@ -1,5 +1,588 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import logout
+
+# from django.contrib.auth.decorators import login_not_required
+from django.contrib.auth.views import LoginView
+
+# from django.core.cache import cache
+# from django.core.management import call_command
+# from django.db import IntegrityError
+# from django.db.models import Avg, Sum
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+
+# from django.utils import timezone
+# from django.views.decorators.csrf import csrf_exempt
+# from rest_framework import serializers
+# from rest_framework.decorators import (
+#     api_view,
+# )
+# # Import Custom Files
+# from .forms import RegistrationForm
+# # Import Custom Files
+# from .forms import RegistrationForm
+# Create your views here.
+from .models import (
+    Session,
+)
+
+# from .utils import logs_to_csv
 
 
-def dashboard(request):
-    return render(request, 'midas/index.html')
+def is_checked_in(request):
+    return (
+        Session.objects.filter(user=request.user, checkout__isnull=True)
+        .order_by('-checkin__time')
+        .exists()
+    )
+
+
+def user_status(request):
+    return 'CHECKED-IN' if is_checked_in(request) else 'CHECKED-OUT'
+
+
+def index(request):
+    sessions = (
+        Session.objects.filter(user=request.user)
+        .select_related('checkin', 'checkout')
+        .order_by('-checkin__time', '-checkout__time')
+        .values(
+            'id',
+            'checkin__time',
+            'checkin__type',
+            'checkout__time',
+            'checkout__type',
+            'checkin__tag__name',
+            'checkout__tag__name',
+        )
+    )
+    return render(
+        request,
+        'midas/index.html',
+        {
+            'sessions': list(sessions),
+            'user_status': user_status(request),
+        },
+    )
+
+
+class LogIn(LoginView):
+    template_name = 'webui/login.html'
+    next_page = reverse_lazy('index')
+
+    def form_valid(self, form):
+        user_name = form.get_user().get_full_name()
+        messages.success(self.request, f'Welcome, comrade {user_name}')
+        return super().form_valid(form)
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'Logged out')
+    return redirect('login')
+
+
+# def serializer_error(serializer):
+#     msg = 'Invalid request:\n'
+#     for field, errors in serializer.errors.items():
+#         if field == 'non_field_errors':
+#             msg += '  general errors:\n'
+#         else:
+#             msg += f'  field {field}:\n'
+#         for error in errors:
+#             msg += f'    {error}\n'
+#     return msg
+
+
+# class RegisterScanSerializer(serializers.Serializer):
+#     device_id = serializers.CharField()
+#     card_id = serializers.CharField()
+
+
+# @csrf_exempt
+# @api_view(['POST'])
+# def register_scan(request):
+#     serializer = RegisterScanSerializer(data=request.data)
+#     if not serializer.is_valid():
+#         return JsonResponse(
+#             {'status': 'error', 'message': serializer_error(serializer)},
+#             status=400,
+#         )
+
+#     card_id = serializer.validated_data['card_id']
+#     scanner_id = serializer.validated_data['device_id']
+
+#     scanner = Scanner.objects.filter(pk=scanner_id).first()
+
+#     if not scanner:
+#         return JsonResponse(
+#             {'status': 'error', 'message': 'Scanner not authorized'}, status=403
+#         )
+
+#     tag = Tag.objects.select_related('owner').filter(tag=card_id).first()
+
+#     if tag is None:
+#         tag = Tag.objects.filter(tag=None).first()
+
+#     if tag is None:
+#         tag = Tag(tag=card_id)
+#         tag.save()
+
+#     log = Log(scanner=scanner, tag=tag)
+
+#     match tag.get_state():
+#         case TagState.UNAUTHORIZED:
+#             log.type = Log.LogEntryType.UNKNOWN
+#             log.save()
+#             return JsonResponse(
+#                 {'status': 'error', 'message': 'Card not registered'},
+#                 status=404,
+#             )
+
+#         case TagState.PENDING_REGISTRATION:
+#             log.type = Log.LogEntryType.REGISTRATION
+#             log.save()
+#             tag.tag = card_id
+#             tag.save()
+#             call_command('update_statistics')
+#             hours_day = (
+#                 Statistics.objects.filter(person=tag.owner).latest('date').minutes_day
+#                 // 60
+#             )
+#             hours_week = (
+#                 Statistics.objects.filter(person=tag.owner).latest('date').minutes_week
+#                 // 60
+#             )
+#             return JsonResponse(
+#                 {
+#                     'state': 'register',
+#                     'owner_name': tag.owner_name(),
+#                     'hours_day': hours_day,
+#                     'hours_week': hours_week,
+#                 }
+#             )
+
+#         case TagState.CLAIMED:
+#             checkout = is_checked_in(tag.owner)
+#             log.type = (
+#                 Log.LogEntryType.CHECKOUT if checkout else Log.LogEntryType.CHECKIN
+#             )
+#             log.save()
+#             call_command('update_statistics')
+#             hours_day = (
+#                 Statistics.objects.filter(person=tag.owner).latest('date').minutes_day
+#                 // 60
+#             )
+#             hours_week = (
+#                 Statistics.objects.filter(person=tag.owner).latest('date').minutes_week
+#                 // 60
+#             )
+#             return JsonResponse(
+#                 {
+#                     'state': 'checkout' if checkout else 'checkin',
+#                     'owner_name': tag.owner_name(),
+#                     'hours_day': hours_day,  # TODO
+#                     'hours_week': hours_week,  # TODO
+#                 }
+#             )
+
+
+# class HealthcheckSerializer(serializers.Serializer):
+#     scanner_id = serializers.CharField()
+
+
+# @api_view(['POST'])
+# def healthcheck(request):
+#     serializer = HealthcheckSerializer(data=request.data)
+#     serializer.is_valid(raise_exception=True)
+
+#     scanner_id = serializer.validated_data['scanner_id']
+
+#     try:
+#         Scanner.objects.get(pk=scanner_id)
+#         return JsonResponse(
+#             {'status': 'ok', 'message': 'Success'},
+#             status=200,
+#         )
+#     except Scanner.DoesNotExist:
+#         return JsonResponse(
+#             {'status': 'error', 'message': 'Scanner not registered'},
+#             status=403,
+#         )
+
+
+# @login_not_required
+# def sign_up(request):
+#     token = request.GET.get('token')
+
+#     # Check if token exists in cache
+#     if not token or not cache.get(f'register_token_{token}'):
+#         return HttpResponseForbidden('Invalid or expired registration link.')
+
+#     if request.method == 'POST':
+#         form = RegistrationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('login')  # replace with your login URL
+#     else:
+#         form = RegistrationForm()
+
+#     return render(request, 'webui/sign_up.html', {'form': form})
+
+
+# def user_statistics(request):
+#     call_command('update_statistics')
+
+#     stats = Statistics.objects.filter(person=request.user).order_by('-date').first()
+
+#     membership = (
+#         Membership.objects.filter_effective()
+#         .select_related('subteam')
+#         .filter(person=request.user)
+#         .first()
+#     )
+
+#     if membership:
+#         quota_minutes_week = membership.job.quota * 60
+#         subteam_name = membership.subteam.name
+#     else:
+#         quota_minutes_week = 9999 * 60
+#         subteam_name = 'You are alone.'
+
+#     # XXX: let's just assume that a month is exactly 4 weeks, noone's gonna notice, right? right?
+#     # TODO: actually calculate amount of workdays in the month
+#     quota_minutes_month = quota_minutes_week * 4
+
+#     quota_week_met = stats.minutes_week >= quota_minutes_week
+#     quota_month_met = stats.minutes_month >= quota_minutes_month
+
+#     def format_time(minutes):
+#         hours = minutes // 60
+#         minutes = minutes % 60
+#         s = f'{hours}h'
+#         if minutes:
+#             s += f' {minutes}m'
+#         return s
+
+#     return render(
+#         request,
+#         'webui/user_statistics.html',
+#         {
+#             # user info
+#             'user_name': request.user.get_full_name(),
+#             'user_role': subteam_name,
+#             'user_status': user_status(request),
+#             # totals
+#             'total_hours_day': format_time(stats.minutes_day),
+#             'total_hours_week': format_time(stats.minutes_week),
+#             'total_hours_month': format_time(stats.minutes_month),
+#             'total_hours_all': format_time(stats.total_minutes),
+#             'average_hours_week': format_time(stats.average_week),
+#             'quota_hours_week': format_time(quota_minutes_week),
+#             # quotas
+#             'quota_week_met': 'MET' if quota_week_met else 'UNMET',
+#             'quota_month_met': 'MET' if quota_month_met else 'UNMET',
+#             # js values (for echarts)
+#             'script_data': {
+#                 'total_hours_week': stats.minutes_week,
+#                 'quota_hours_week': quota_minutes_week,
+#             },
+#         },
+#     )
+
+
+# class AddTagSerializer(serializers.Serializer):
+#     tag_name = serializers.CharField()
+
+
+# class ScanTagSerializer(serializers.Serializer):
+#     tag = serializers.IntegerField()
+
+
+# def user_profile(request):
+#     if request.POST.get('action') == 'add_tag':
+#         serializer = AddTagSerializer(data=request.POST)
+#         serializer.is_valid(raise_exception=True)
+
+#         tag_name = serializer.validated_data['tag_name']
+
+#         tag = Tag(name=tag_name, owner=request.user)
+#         tag.save()
+
+#         return redirect(
+#             reverse_lazy('user_profile', query={'modal': 'tag_scan', 'tag': tag.id})
+#         )
+
+#     if request.GET.get('modal') == 'tag_scan':
+#         serializer = ScanTagSerializer(data=request.GET)
+#         serializer.is_valid(raise_exception=True)
+
+#         tag_code = serializer.validated_data['tag']
+
+#         tag = get_object_or_404(Tag, pk=tag_code)
+
+#         if tag.get_state() == TagState.CLAIMED:
+#             return redirect('user_profile')
+
+#     tags = Tag.objects.get_authorized().filter(owner=request.user)
+#     membership = (
+#         Membership.objects.filter(person=request.user)
+#         .select_related('job', 'subteam')
+#         .order_by('-starting_from')
+#         .first()
+#     )
+
+#     return render(
+#         request,
+#         'webui/user_profile.html',
+#         {
+#             'jobs': Job.objects.all(),
+#             'membership': membership,
+#             'modal_name': request.GET.get('modal'),
+#             'request': request,
+#             'subteams': SubTeam.objects.all(),
+#             'tags': tags,
+#             'user': request.user,
+#             'user_status': user_status(request),
+#         },
+#     )
+
+
+# class EditMembershipSerializer(serializers.Serializer):
+#     first_name = serializers.CharField()
+#     last_name = serializers.CharField()
+#     username = serializers.CharField()
+#     job = serializers.IntegerField()
+#     subteam = serializers.IntegerField()
+
+
+# def edit_profile(request):
+#     serializer = EditMembershipSerializer(data=request.POST)
+#     if not serializer.is_valid():
+#         messages.error(request, 'Please fill all fields')
+#         return redirect('user_profile')
+
+#     first_name = serializer.validated_data['first_name']
+#     last_name = serializer.validated_data['last_name']
+#     username = serializer.validated_data['username']
+#     job_id = serializer.validated_data['job']
+#     subteam_id = serializer.validated_data['subteam']
+
+#     job = get_object_or_404(Job, pk=job_id)
+#     subteam = get_object_or_404(SubTeam, pk=subteam_id)
+
+#     current_membership = (
+#         Membership.objects.filter_effective()
+#         .filter(person=request.user)
+#         .order_by('starting_from')
+#         .last()
+#     )
+
+#     request.user.first_name = first_name
+#     request.user.last_name = last_name
+#     request.user.username = username
+#     try:
+#         request.user.save()
+#     except IntegrityError:
+#         messages.error(request, f'Username {username} is already taken!')
+#         return redirect('user_profile')
+
+#     if (
+#         not current_membership
+#         or not current_membership.job
+#         or not current_membership.subteam
+#         or current_membership.job.id != job.id
+#         or current_membership.subteam.id != subteam.id
+#     ):
+#         Membership.objects.create(
+#             person=request.user,
+#             job=job,
+#             subteam=subteam,
+#         )
+
+#     messages.success(request, 'Profile updated')
+#     return redirect('user_profile')
+
+
+# class DeleteTagSerializer(serializers.Serializer):
+#     tag_id = serializers.IntegerField()
+
+
+# def delete_tag(request):
+#     serializer = DeleteTagSerializer(data=request.POST)
+#     serializer.is_valid(raise_exception=True)
+
+#     tag_id = serializer.validated_data['tag_id']
+
+#     tag = get_object_or_404(Tag, pk=tag_id)
+
+#     if tag.name == 'WebUI':
+#         return JsonResponse(
+#             {'status': 'error', 'message': 'Cannot delete WebUI tag'},
+#             status=403,
+#         )
+
+#     tag.name = ''
+#     tag.save()
+
+#     return redirect('user_profile')
+
+
+# class RenameTagSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     tag_name = serializers.CharField(max_length=100)
+
+
+# def rename_tag(request):
+#     serializer = RenameTagSerializer(data=request.POST)
+#     serializer.is_valid(raise_exception=True)
+
+#     tag_code = serializer.validated_data['id']
+#     tag_name = serializer.validated_data['tag_name']
+
+#     tag = get_object_or_404(Tag, pk=tag_code)
+
+#     if tag.name == 'WebUI':
+#         return JsonResponse(
+#             {'status': 'error', 'message': 'Cannot edit WebUI tag'}, status=403
+#         )
+#     tag.name = tag_name
+#     tag.save()
+#     return redirect('user_profile')
+
+
+# @api_view(['GET'])
+# def export_user_logs(request):
+#     qs = (
+#         Log.objects.filter(tag__owner=request.user)
+#         .select_related('tag', 'scanner')
+#         .order_by('-time')
+#     )
+#     return logs_to_csv(qs)
+
+
+# @api_view(['GET'])
+# def check_registration(request, db_id):
+#     tag = Tag.objects.filter(id=db_id, owner=request.user).first()
+#     if tag is None:
+#         return JsonResponse({'status': 'error', 'message': 'Not found'}, status=404)
+
+#     state = tag.get_state()
+#     state_str = state.value if state else None
+#     return JsonResponse(
+#         {
+#             'status': 'success',
+#             'claimed': state == TagState.CLAIMED,
+#             'state': state_str,
+#         }
+#     )
+
+
+# @api_view(['GET'])
+# def user_tags(request):
+#     tags = Tag.objects.filter(owner=request.user).all()
+
+#     data = []
+#     for tag in tags:
+#         state = tag.get_state()
+#         state_str = state.value if state else None
+#         data.append(
+#             {
+#                 'tag_code': str(tag.tag) if tag.tag is not None else None,
+#                 'tag_name': str(tag.name) if tag.name is not None else None,
+#                 'tag_owner': tag.owner_name(),
+#                 'tag_state': state_str,
+#             }
+#         )
+
+#     return JsonResponse({'status': 'success', 'tags': data}, status=200)
+
+
+# def fuel_guage(request):
+#     try:
+#         membership = Membership.objects.filter(person=request.user).first()
+#         if not membership or not membership.job:
+#             return JsonResponse(
+#                 {
+#                     'status': 'error',
+#                     'message': 'No job assignment found for user.',
+#                 },
+#                 status=400,
+#             )
+#         member_quota = membership.job.quota
+#         stats = Statistics.objects.filter(person=request.user).order_by('-date').first()
+#         hours_week = stats.minutes_week / 60
+
+#         percentage = (hours_week / member_quota) * 100
+
+#         return JsonResponse(
+#             {
+#                 'status': 'Success',
+#                 'data': {
+#                     'quota': member_quota,
+#                     'hours_week': hours_week,
+#                     'amt_work_done': percentage,
+#                 },
+#             }
+#         )
+#     except Exception as e:
+#         return JsonResponse(
+#             {'status': 'error', 'message': f'Error retrieving data: {str(e)}'},
+#             status=500,
+#         )
+
+
+# class AutoCheckoutSerializer(serializers.Serializer):
+#     checkout_time = serializers.DateTimeField(required=True)
+
+
+# @api_view(['POST'])
+# def auto_checkout(request):
+#     serializer = AutoCheckoutSerializer(data=request.data)
+#     if not serializer.is_valid():
+#         return JsonResponse(
+#             {'status': 'error', 'message': serializer_error(serializer)},
+#             status=400,
+#         )
+
+#     checkout_time = serializer.validated_data['checkout_time']
+#     current_time = timezone.now()
+
+#     if checkout_time > current_time:
+#         return JsonResponse(
+#             {
+#                 'status': 'error',
+#                 'message': 'Checkout time cannot be in the future.',
+#             },
+#             status=400,
+#         )
+
+#     if not is_checked_in(request.user):
+#         return JsonResponse(
+#             {'status': 'error', 'message': 'User is not currently checked in.'},
+#             status=400,
+#         )
+
+#     tag = Tag.objects.filter(owner=request.user, name='WebUI').first()
+#     if not tag:
+#         return JsonResponse(
+#             {'status': 'error', 'message': 'No valid tag found for user.'},
+#             status=404,
+#         )
+
+#     log = Log.objects.create(
+#         type=Log.LogEntryType.CHECKOUT,
+#         tag=tag,
+#         time=checkout_time,
+#     )
+
+#     return JsonResponse(
+#         {
+#             'status': 'success',
+#             'state': log.type,
+#             'state_display': log.get_type_display(),
+#             'date': log.time.isoformat(),
+#             'tag': str(tag),
+#         },
+#         status=201,
+#     )
