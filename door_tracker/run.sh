b53ef976f008123f9e349c49a00881ca6061f6f8
jarkad <jarkad@jarkad.net.eu.org>
2026-06-17 17:27:37 +0200

    feat(build): clarify what door_tracker/run.sh is

--- door_tracker/run.sh
+++ door_tracker/run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -eux
 
+# This is the entry point of the Docker container.
+# For non-containerized development, run `just dev` instead.
+
 export DJANGO_SETTINGS_MODULE=door_tracker.settings
 django-admin migrate
 django-admin init_admin
